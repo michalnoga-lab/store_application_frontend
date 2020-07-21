40b6b2a994437420695823710a610b5b22bfcc9b
Michal <mscislaw@poczta.fm>
2020-07-21 23:15:49 +0200

    Product search engine optimized

--- src/components/_user/search/ProductSearch.jsx
+++ src/components/_user/search/ProductSearch.jsx
@@ -1,7 +1,4 @@
 import React, {Component} from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-
 import * as URLs from '../../URLs'
 import Table from "react-bootstrap/Table";
 
@@ -11,54 +8,38 @@ class ProductSearch extends Component {
         super(props);
 
         this.state = {
-            searchQuery: '',
-            foundedProducts: []
+            products: []
         }
     }
 
-    updateSearchQuery = e => {
-        this.setState({searchQuery: e.target.value})
-    }
+    filter = e => this.setState({filter: e.target.value});
 
-    async searchForProduct() {
-
-        const url = URLs.backend + 'api/search/products';
+    componentDidMount() {
+        const url = URLs.backend + 'api/products/all';
         const headers = new Headers();
         headers.set('Content-Type', 'application/json;charset=UTF-8');
         headers.set('Authorization', 'Bearer ' + sessionStorage.getItem('token'));
 
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify({userInput: this.state.searchQuery})
-        });
-
-        const body = await response.json();
-        this.setState({foundedProducts: await body});
-    }
-
-    clearSearchQuery() {
-        this.setState({foundedProducts: []})
+        fetch(url, {
+            method: 'GET',
+            headers: headers
+        })
+            .then(response => response.json())
+            .then(products => this.setState({products: products}));
     }
 
     render() {
+        const allProducts = this.state.products;
         let rowNumber = 0;
+        let products = allProducts;
 
-        if (this.state.foundedProducts.length === 0) {
-            return (
-                <div className="input-page">
-                    <Form>
-                        <Form.Group controlId="searchForm">
-                            <Form.Control type="text" placeholder='NAZWA PRODUKTU'
-                                          onChange={this.updateSearchQuery}/>
-                        </Form.Group>
-                    </Form>
-                    <Button onClick={() => this.searchForProduct(this.state.searchQuery)}
-                            variant="outline-secondary btn-block" type="button">WYSZUKAJ</Button>
-                </div>
-            )
-        } else {
-            return (
+        if (this.state.filter) {
+            products = allProducts.filter(product => product.name.toString().toLowerCase().includes(this.state.filter.toLowerCase()));
+        }
+
+        return (<div className="input-page">
+                <input type='text' onChange={this.filter} className='btn-block' placeholder='SZUKANA NAZWA'/>
+
                 <div className="table-page">
                     <Table bordered hover>
                         <thead>
@@ -69,22 +50,21 @@ class ProductSearch extends Component {
                         </tr>
                         </thead>
                         <tbody>
-                        {this.state.foundedProducts.map(el => (
-                            <tr key={el.id}>
-                                <td>{rowNumber+=1}</td>
-                                <td>{el.name}</td>
-                                <td>{el.nettPrice} PLN</td>
-                                {/*todo formatowanie z zerami po przecinku*/}
-                            </tr>
-                        ))}
+                        {products.map(product => <ProductItem key={product.id} rowNumber={rowNumber += 1}
+                                                              product={product}/>)}
                         </tbody>
                     </Table>
-                    <Button onClick={() => this.clearSearchQuery()} variant="outline-secondary btn-block" type="button">WYSZUKAJ
-                        PONOWNIE</Button>
                 </div>
-            )
-        }
+            </div>
+        )
     }
 }
+
+const ProductItem = props =>
+    <tr>
+        <td>{props.rowNumber}</td>
+        <td>{props.product.name}</td>
+        <td>{props.product.nettPrice} PLN</td>
+    </tr>
 
 export {ProductSearch}

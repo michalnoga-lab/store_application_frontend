2cb396e78b5531d83c9d18d617ba76952d55b654
Michal <mscislaw@poczta.fm>
2020-09-21 12:18:48 +0200

    Add delivery address with HOOKs

--- src/components/_user/deliveryAddress/AddDeliveryAddress.jsx
+++ src/components/_user/deliveryAddress/AddDeliveryAddress.jsx
@@ -1,67 +1,53 @@
-import React, {Component} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import * as URLs from '../../URLs'
 
-class AddDeliveryAddress extends Component {
+const AddDeliveryAddress = () => {
 
-    constructor(props) {
-        super(props);
+    const [street, setStreet] = useState('')
+    const [phone, setPhone] = useState('')
+    const [isStreetCorrect, setIsStreetCorrect] = useState(false)
+    const [streetErrorMessage, setStreetErrorMessage] = useState('Dozwolone są tylko litery i cyfry')
+    const [isPhoneCorrect, setIsPhoneCorrect] = useState(false)
+    const [phoneErrorMessage, setPhoneErrorMessage] = useState('Dozwolone są tylko litery i cyfry')
+    const [isAddressAdded, setIsAddressAdded] = useState(false)
+    const [addressAddedMessage, setAddressAddedMessage] = useState('Poprawnie dodano nowy adres')
 
-        this.state = {
-            street: '',
-            phone: '',
-            isStreetIncorrect: false,
-            streetErrorMessage: '',
-            isPhoneIncorrect: false,
-            phoneErrorMessage: '',
-            isAddressAdded: false,
-            addressAddedMessage: ''
+    useEffect(() => {
+        if (Object.keys(street).length === 0) {
+            setIsStreetCorrect(true)
         }
-    }
+        if (Object.keys(phone).length === 0) {
+            setIsPhoneCorrect(true)
+        }
+    })
 
-    handleChange = event => {
-        let target = event.target;
-        let value = target.value;
-        let name = target.name;
+    const handleStreetChange = event => {
+        let target = event.target
+        let value = target.value
         const regex = /^[a-z0-9\s]{0,100}$/g;
-        const errorMessage = 'Dozwolone są tylko litery i cyfry';
 
-        if (name === 'street') {
-            if (regex.test(value)) {
-                this.setState({isStreetIncorrect: false})
-            } else {
-                this.setState({isStreetIncorrect: true, streetErrorMessage: errorMessage, isAddressAdded: false});
-            }
-        }
-        if (name === 'phone') {
-            if (regex.test(value)) {
-                this.setState({isPhoneIncorrect: false})
-            } else {
-                this.setState({isPhoneIncorrect: true, phoneErrorMessage: errorMessage, isAddressAdded: false})
-            }
-        }
-        this.setState({[name]: value});
+        regex.test(value) ? setIsStreetCorrect(true) : setIsStreetCorrect(false)
+        setStreet(value)
     }
 
-    showAddressAddedMessage = () => {
-        this.setState({
-            isAddressAdded: true,
-            addressAddedMessage: 'Poprawnie dodano nowy adres'
-        })
+    const handlePhoneChange = event => {
+        let target = event.target
+        let value = target.value
+        const regex = /^[a-z0-9\s]{0,100}$/g;
+
+        regex.test(value) ? setIsPhoneCorrect(true) : setIsPhoneCorrect(false)
+        setPhone(value)
     }
 
-    clearFields = () => {
-        this.setState({
-            street: '',
-            phone: '',
-            isStreetIncorrect: false,
-            streetErrorMessage: '',
-            isPhoneIncorrect: false,
-            phoneErrorMessage: ''
-        })
+    const clearFields = () => {
+        setStreet('')
+        setPhone('')
+        setIsStreetCorrect(false)
+        setIsPhoneCorrect(false)
     }
 
-    handleSubmit = event => {
-        if (!this.state.isStreetIncorrect && !this.state.isPhoneIncorrect) {
+    const handleSubmit = event => {
+        if (isStreetCorrect && isPhoneCorrect) {
 
             const url = URLs.backend + 'api/deliveryAddress/add';
             const headers = new Headers();
@@ -71,54 +57,53 @@ class AddDeliveryAddress extends Component {
             fetch(url, {
                 method: 'POST',
                 headers: headers,
-                body: JSON.stringify({street: this.state.street, phone: this.state.phone})
+                body: JSON.stringify({street: street, phone: phone})
             })
-                .then(this.clearFields)
-                .then(this.showAddressAddedMessage);
+                .then(clearFields)
+            setIsAddressAdded(true)
         }
         event.preventDefault();
     }
 
-    render() {
-        return (
-            <div className='input-page'>
-                <form onSubmit={this.handleSubmit}>
-                    <div className='form-group'>
-                        <input
-                            className='btn-block form-control'
-                            type='text'
-                            placeholder='ULICA'
-                            name='street'
-                            value={this.state.street}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div className='alert alert-danger' hidden={!this.state.isStreetIncorrect}>
-                        {this.state.streetErrorMessage}
-                    </div>
-                    <div className='form-group'>
-                        <input
-                            className='btn-block form-control'
-                            type='text'
-                            placeholder='TELEFON'
-                            name='phone'
-                            value={this.state.phone}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <div className='alert alert-danger' hidden={!this.state.isPhoneIncorrect}>
-                        {this.state.phoneErrorMessage}
-                    </div>
-                    <div className='alert alert-success' hidden={!this.state.isAddressAdded}>
-                        {this.state.addressAddedMessage}
-                    </div>
-                    <div>
-                        <button type='submit' className='btn btn-block btn-outline-dark'>DODAJ</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div className='input-page'>
+            <form onSubmit={handleSubmit}>
+                <div className='form-group'>
+                    <input
+                        className='btn-block form-control'
+                        type='text'
+                        placeholder='ULICA'
+                        name='street'
+                        value={street}
+                        onChange={handleStreetChange}
+                    />
+                </div>
+                <div className='alert alert-danger' hidden={isStreetCorrect}>
+                    {streetErrorMessage}
+                </div>
+                <div className='form-group'>
+                    <input
+                        className='btn-block form-control'
+                        type='text'
+                        placeholder='TELEFON'
+                        name='phone'
+                        value={phone}
+                        required={true}
+                        onChange={handlePhoneChange}
+                    />
+                </div>
+                <div className='alert alert-danger' hidden={isPhoneCorrect}>
+                    {phoneErrorMessage}
+                </div>
+                <div className='alert alert-success' hidden={!isAddressAdded}>
+                    {addressAddedMessage}
+                </div>
+                <div>
+                    <button type='submit' className='btn btn-block btn-outline-dark'>DODAJ</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export {AddDeliveryAddress}
--- src/components/_user/deliveryAddress/DeliveryAddress.jsx
+++ src/components/_user/deliveryAddress/DeliveryAddress.jsx
@@ -77,3 +77,96 @@ const DeliveryAddress = () => {
 }
 
 export {DeliveryAddress}
+
+// import React, {Component} from "react";
+// import Table from "react-bootstrap/Table";
+// import * as URLs from '../../URLs'
+//
+// class DeliveryAddress extends Component {
+//
+//     constructor(props) {
+//         super(props);
+//
+//         this.state = {
+//             deliveryAddresses: []
+//         }
+//     }
+//
+//     filterStreet = e => this.setState({filterStreet: e.target.value});
+//
+//     getAllDeliveryAddresses = () => {
+//         const url = URLs.backend + 'api/deliveryAddress/all';
+//         const headers = new Headers();
+//         headers.set('Content-Type', 'application/json;charset=UTF-8');
+//         headers.set('Authorization', 'Bearer ' + sessionStorage.getItem('token'));
+//
+//         fetch(url, {
+//             method: 'GET',
+//             headers: headers
+//         }).then(response => response.json())
+//             .then(deliveryAddresses => this.setState({deliveryAddresses: deliveryAddresses}));
+//     }
+//
+//     componentDidMount() {
+//         this.getAllDeliveryAddresses();
+//     }
+//
+//     render() {
+//         const allDeliveryAddresses = this.state.deliveryAddresses;
+//         let rowNumber = 0;
+//         let deliveryAddresses = allDeliveryAddresses;
+//
+//         if (this.state.filterStreet) {
+//             deliveryAddresses = allDeliveryAddresses.filter(address =>
+//                 address.street.toString().toLowerCase().includes(this.state.filterStreet.toString().toLowerCase()));
+//         }
+//
+//         if (Object.entries(allDeliveryAddresses).length === 0) {
+//             return (
+//                 <EmptyList/>
+//             )
+//         } else {
+//             return (
+//                 <div className='table-page'>
+//                     <Table bordered hover>
+//                         <thead>
+//                         <tr>
+//                             <td>Lp</td>
+//                             <td><input type='text' onChange={this.filterStreet} className='btn-block'
+//                                        placeholder='WYSZUKAJ PO ULICY'/></td>
+//                             <td>Telefon</td>
+//                         </tr>
+//                         </thead>
+//                         <tbody>
+//                         {deliveryAddresses.map(deliveryAddress => <AddressItem key={deliveryAddress.id}
+//                                                                                address={deliveryAddress}
+//                                                                                rowNumber={rowNumber += 1}/>)}
+//                         </tbody>
+//                     </Table>
+//                 </div>
+//             )
+//         }
+//     }
+// }
+//
+//
+// const EmptyList = () => (
+//     <div className='main-page'>
+//         <section className="container">
+//             <h5 className="top-page-text">MOJE ADRESY</h5>
+//             <div className="top-page-text-details">
+//                 <p className="top-page-text-details-at">@ status</p>
+//                 <p className="top-page-text-details-text">nie masz jeszcze zapisanych żadnych adresów</p>
+//             </div>
+//         </section>
+//     </div>
+// )
+//
+// const AddressItem = props =>
+//     <tr>
+//         <td>{props.rowNumber}</td>
+//         <td>{props.address.street}</td>
+//         <td>{props.address.phone}</td>
+//     </tr>
+//
+// export {DeliveryAddress}
